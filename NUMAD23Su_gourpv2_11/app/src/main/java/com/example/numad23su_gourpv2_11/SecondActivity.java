f92882919dcb68d10e8d9b4c7bcb942c44f07b44
Lalit Kishore <lalitkishore12@gmail.com>
2023-06-16 10:27:31 -0400

    added weather map api and relevant buttons

--- NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/SecondActivity.java
+++ NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/SecondActivity.java
@@ -1,13 +1,79 @@
 package com.example.numad23su_gourpv2_11;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class SecondActivity extends AppCompatActivity {
+    private static final String API_KEY = "805749b9bcdde60cbe02310708cdfe0c";
+    private EditText cityName;
+    private Button fetchButton;
+    private TextView resultTextView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        cityName = findViewById(R.id.city_name);
+        fetchButton = findViewById(R.id.fetch_button);
+        resultTextView = findViewById(R.id.result_text_view);
+        progressBar = findViewById(R.id.progress_circular);
+
+        fetchButton.setOnClickListener(view -> fetchWeatherData());
+    }
+
+    private void fetchWeatherData() {
+        String city = cityName.getText().toString();
+
+        Thread thread = new Thread(() -> {
+            try {
+                runOnUiThread(() -> progressBar.setVisibility(View.VISIBLE));
+                URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + API_KEY);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line;
+                StringBuilder result = new StringBuilder();
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+                rd.close();
+                parseJsonData(result.toString());
+            } catch (Exception e) {
+                Log.e("SecondActivity", "Error fetching weather data", e);
+            } finally {
+                runOnUiThread(() -> progressBar.setVisibility(View.GONE));
+            }
+        });
+        thread.start();
+    }
+
+
+    private void parseJsonData(String jsonData) {
+        try {
+            JSONObject obj = new JSONObject(jsonData);
+            JSONObject main = obj.getJSONObject("main");
+            final String temperature = main.getString("temp");
+            final String humidity = main.getString("humidity");
+
+            runOnUiThread(() -> resultTextView.setText("Temperature: " + temperature + "\n" + "Humidity: " + humidity));
+        } catch (Exception e) {
+            Log.e("SecondActivity", "Error parsing JSON", e);
+        }
     }
 }
