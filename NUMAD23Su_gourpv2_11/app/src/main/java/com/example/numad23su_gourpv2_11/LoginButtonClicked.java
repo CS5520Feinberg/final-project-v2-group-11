9b9752a27ea9095a7fbe5c5a4d7647088abac016
Lalit Kishore <lalitkishore12@gmail.com>
2023-06-30 11:25:25 -0400

    added login buutton and username editext and a new activity for when login btn is clicked

---
+++ NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/LoginButtonClicked.java
@@ -0,0 +1,14 @@
+package com.example.numad23su_gourpv2_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginButtonClicked extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_button_clicked);
+    }
+}
--- NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/StickItToEm.java
+++ NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/StickItToEm.java
@@ -2,7 +2,11 @@ package com.example.numad23su_gourpv2_11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class StickItToEm extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class StickItToEm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stick_it_to_em);
+
+        Button loginButton = findViewById(R.id.loginBtn);
+        EditText username = findViewById(R.id.Username);
+
+        loginButton.setOnClickListener(view -> loginButtonClicked());
+    }
+
+    public void loginButtonClicked () {
+        Intent intent = new Intent(StickItToEm.this , LoginButtonClicked.class);
+        startActivity(intent);
     }
 }
