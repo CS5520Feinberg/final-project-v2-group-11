f62dcde38f5ea5300c752e448ca36fa5e953b479
yangd <yang.dani@northeastern.edu>
2023-07-14 04:30:34 -0400

    Started project. Home page added, buttons to other activties added

--- NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/MainActivity.java
+++ NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/MainActivity.java
@@ -8,6 +8,7 @@ package com.example.numad23su_gourpv2_11;
 
         import com.example.numad23su_gourpv2_11.AtYourService.SecondActivity;
         import com.example.numad23su_gourpv2_11.StickItToEm.StickItToEm;
+        import com.example.numad23su_gourpv2_11.TourGuide.TourGuide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,16 +18,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button openSecondActivityButton = findViewById(R.id.button_open_second_activity);
-        Button stickItToEmBtn = findViewById(R.id.stickItToEmButton);
+        Button stickItToEmButton = findViewById(R.id.stickItToEmButton);
+        Button tourGuideButton = findViewById(R.id.tourGuide);
 
         openSecondActivityButton.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
             startActivity(intent);
         });
 
-        stickItToEmBtn.setOnClickListener(view -> {
-            Intent intent1 = new Intent(MainActivity.this, StickItToEm.class);
-            startActivity(intent1);
+        stickItToEmButton.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, StickItToEm.class);
+            startActivity(intent);
+        });
+
+        tourGuideButton.setOnClickListener(view ->{
+            Intent intent = new Intent(MainActivity.this, TourGuide.class);
+            startActivity(intent);
         });
     }
 }
