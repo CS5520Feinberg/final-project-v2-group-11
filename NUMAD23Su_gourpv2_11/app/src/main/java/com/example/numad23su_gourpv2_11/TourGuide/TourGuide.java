ed3bab865a588d54280b4d5c3841fb525f0cff90
Lalit Kishore <lalitkishore12@gmail.com>
2023-07-23 16:06:00 -0400

    fixed a bug with the homepage

--- NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/TourGuide/TourGuide.java
+++ NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/TourGuide/TourGuide.java
@@ -3,11 +3,15 @@ package com.example.numad23su_gourpv2_11.TourGuide;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.numad23su_gourpv2_11.R;
+
 public class TourGuide extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tour_guide_home_screen);
 
 
     }
