5ed334386c0df64187a4b0a79b75fa5060f9e796
Lalit Kishore <lalitkishore12@gmail.com>
2023-07-25 18:57:41 -0400

    Added new activity classes for each of the button pressed from Freedom trail, resturants and parks.

---
+++ NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/TourGuide/FreedomTrail.java
@@ -0,0 +1,16 @@
+package com.example.numad23su_gourpv2_11.TourGuide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.numad23su_gourpv2_11.R;
+
+public class FreedomTrail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_freedom_trail);
+    }
+}
--- NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/TourGuide/TourGuide.java
+++ NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/TourGuide/TourGuide.java
@@ -1,6 +1,8 @@
 package com.example.numad23su_gourpv2_11.TourGuide;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,11 +10,34 @@ import com.example.numad23su_gourpv2_11.R;
 
 public class TourGuide extends AppCompatActivity {
 
+    Button freedomtrailBtn, resturantsBtn, parksBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tour_guide_home_screen);
 
+        freedomtrailBtn = findViewById(R.id.freedom_trail_button);
+        resturantsBtn = findViewById(R.id.restaurants_button);
+        parksBtn = findViewById(R.id.parks_button);
+
+        freedomtrailBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(TourGuide.this, FreedomTrail.class);
+            startActivity(intent);
+        });
+
+        resturantsBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(TourGuide.this, RestaurantsActivity.class);
+            startActivity(intent);
+        });
+
+        parksBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(TourGuide.this, ParksActivity.class);
+            startActivity(intent);
+        });
 
     }
+
+
+
 }
