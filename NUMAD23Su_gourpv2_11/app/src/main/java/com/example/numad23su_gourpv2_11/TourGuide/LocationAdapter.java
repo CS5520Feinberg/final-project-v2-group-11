417f2fc2641ecee4669bea46c07ea7bc16f80449
Lalit Kishore <lalitkishore12@gmail.com>
2023-08-04 13:45:08 -0400

    made changes to the latitude and longitude textview, added a new text view which shows a material UI alert dialog box for the cooridinates

--- NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/TourGuide/LocationAdapter.java
+++ NUMAD23Su_gourpv2_11/app/src/main/java/com/example/numad23su_gourpv2_11/TourGuide/LocationAdapter.java
@@ -1,5 +1,6 @@
 package com.example.numad23su_gourpv2_11.TourGuide;
 
+import android.annotation.SuppressLint;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
@@ -9,21 +10,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.numad23su_gourpv2_11.R;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-import java.net.URI;
 import java.util.ArrayList;
 
 public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHolder> {
-    private ArrayList<LocationClass> locations;
+    private final ArrayList<LocationClass> locations;
 
     // constructor
     public LocationAdapter(ArrayList<LocationClass> locations) {
         this.locations = locations;
     }
 
+    @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
@@ -49,12 +52,12 @@ public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHo
         public TextView nameTextView;
         public TextView descTextView;
         public TextView addressTextView;
-        public TextView latitudeTextView;
-        public TextView longitudeTextView;
 
         public TextView phoneTextView;
 
         public TextView urlTextView;
+
+        public TextView checkCoordinates;
 
 
         public ViewHolder(View v) {
@@ -62,79 +65,68 @@ public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.ViewHo
             nameTextView = v.findViewById(R.id.nameTextView);
             descTextView = v.findViewById(R.id.descriptionTextView);
             addressTextView = v.findViewById(R.id.addressTextView);
-            latitudeTextView = v.findViewById(R.id.latitudeTextView);
-            longitudeTextView = v.findViewById(R.id.longitudeTextView);
             phoneTextView = v.findViewById(R.id.phoneTextView);
             urlTextView = v.findViewById(R.id.urlTextView);
+            checkCoordinates = v.findViewById(R.id.checkCoordinates);
 
-            phoneTextView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String phoneNumberWithPrefix = ((TextView) v).getText().toString();
-                    String phoneNumber = phoneNumberWithPrefix.replace("Phone: ", "");
-                    Intent dial = new Intent(Intent.ACTION_DIAL);
-                    dial.setData(Uri.parse("tel:" + phoneNumber));
-                    v.getContext().startActivity(dial);
-                }
+            phoneTextView.setOnClickListener(v1 -> {
+                String phoneNumberWithPrefix = ((TextView) v1).getText().toString();
+                String phoneNumber = phoneNumberWithPrefix.replace("Phone: ", "");
+                Intent dial = new Intent(Intent.ACTION_DIAL);
+                dial.setData(Uri.parse("tel:" + phoneNumber));
+                v1.getContext().startActivity(dial);
             });
 
         }
 
+        @SuppressLint({"SetTextI18n", "QueryPermissionsNeeded"})
         public void bind(LocationClass location) {
             nameTextView.setText(location.getName());
             descTextView.setText(location.getDescription());
             addressTextView.setText("Address: " + location.getAddress());
-            latitudeTextView.setText("Latitude: " + location.getLatitude());
-            longitudeTextView.setText("Longitude: " + location.getLongitude());
             phoneTextView.setText("Phone: " + location.getPhone());
 
-            urlTextView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String url = location.getURLlink();
-                    //String url = ((TextView) view).getText().toString();
-                    if (!url.startsWith("http://") && !url.startsWith("https://")) {
-                        url = "http://" + url;
-                    }
-//                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-//                    if (browserIntent.resolveActivity(view.getContext().getPackageManager()) != null) {
-//                        view.getContext().startActivity(browserIntent);
-//                    } else {
-//                        Log.d("URL Handler", "No Intent available to handle action");
-//                    }
-                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                    browserIntent.setPackage("com.android.chrome");
-                    try {
-                        view.getContext().startActivity(browserIntent);
-                    } catch (ActivityNotFoundException ex) {
-                        // Chrome browser presumably not installed and open URL in default browser
-                        browserIntent.setPackage(null);
-                        view.getContext().startActivity(browserIntent);
-                    }
-
+            urlTextView.setOnClickListener(view -> {
+                String url = location.getURLlink();
+                //String url = ((TextView) view).getText().toString();
+                if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                    url = "http://" + url;
                 }
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                browserIntent.setPackage("com.android.chrome");
+                try {
+                    view.getContext().startActivity(browserIntent);
+                } catch (ActivityNotFoundException ex) {
+                    // Chrome browser presumably not installed and open URL in default browser
+                    browserIntent.setPackage(null);
+                    view.getContext().startActivity(browserIntent);
+                }
+
             });
 
             try {
                 final double latitude = Double.parseDouble(location.getLatitude());
                 final double longitude = Double.parseDouble(location.getLongitude());
 
-                nameTextView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Uri gmmIntentUri = Uri.parse("geo:" + latitude + "," + longitude);
-                        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-                        mapIntent.setPackage("com.google.android.apps.maps");
-                        if (mapIntent.resolveActivity(v.getContext().getPackageManager()) != null) {
-                            v.getContext().startActivity(mapIntent);
-                        } else {
-                            Log.d("LocationAdapter", "No Intent available to handle action");
-                        }
+                nameTextView.setOnClickListener(v -> {
+                    Uri gmmIntentUri = Uri.parse("geo:" + latitude + "," + longitude);
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    if (mapIntent.resolveActivity(v.getContext().getPackageManager()) != null) {
+                        v.getContext().startActivity(mapIntent);
+                    } else {
+                        Log.d("LocationAdapter", "No Intent available to handle action");
                     }
                 });
             } catch (NumberFormatException e) {
                 Log.e("LocationAdapter", "Could not parse latitude/longitude", e);
             }
+
+            checkCoordinates.setOnClickListener(view -> new MaterialAlertDialogBuilder(view.getContext())
+                    .setTitle("Coordinates")
+                    .setMessage("Latitude: " + location.getLatitude() + "\nLongitude: " + location.getLongitude())
+                    .setPositiveButton("OK", /* listener = */ null)
+                    .show());
         }
     }
 }
